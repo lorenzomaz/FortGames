1ed009808f7ef3a9a9adaff498ca6a944c40f984
Elliott Migotti <elliottmigotti@gmail.com>
2022-08-03 17:48:34 +0200

    Account Controller Base: Fatto

---
+++ FortGames.Domain/Models/RegisterModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace FortGames.Domain.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        [StringLength(100)]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [JsonIgnore]
+        public bool EmailConfirmed { get; set; } = true;
+
+    }
+}
---
+++ FortGames.Infrastructure/Extensions/UserExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Claims;
+using System.Security.Principal;
+
+namespace FortGames.Infrastructure.Extensions
+{
+    public static class UserExtensions
+    {
+        public static string GetUserId(this IPrincipal principal, string claimType = ClaimTypes.NameIdentifier)
+        {
+            var claimsIdentity = (ClaimsIdentity)principal.Identity!;
+            var claim = claimsIdentity.FindFirst(claimType);
+            return claim!.Value;
+        }
+    }
+}
