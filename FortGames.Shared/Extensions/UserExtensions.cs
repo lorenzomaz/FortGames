5212fabfa878095dd3b1a8ae74a2baa6fb708fc1
lorenzomaz <hello@lorenzomazza.dev>
2022-08-03 22:33:47 +0200

    okokokok

---
+++ FortGames.Shared/Extensions/UserExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+using System.Security.Principal;
+
+namespace FortGames.Shared.Extensions
+{
+    public static class UserExtensions
+    {
+        public static string GetUserId(this IPrincipal principal, string claimType = ClaimTypes.NameIdentifier)
+        {
+            var claimsIdentity = (ClaimsIdentity)principal.Identity!;
+            var claim = claimsIdentity.FindFirst(claimType);
+            return claim!.Value;
+        }
+    }
+}
+
