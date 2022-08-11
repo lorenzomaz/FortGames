bd7dca49e15bac9fd290e4c407ff9ae0af839fae
Delirivs <elliottmigotti@gmail.com>
2022-08-11 16:53:39 +0200

    Edit Game DONE!
    still can't believe it

--- FortGames.Web/src/app/features/edit-games/edit-games-dialog/edit-games-dialog.component.ts
+++ FortGames.Web/src/app/features/edit-games/edit-games-dialog/edit-games-dialog.component.ts
@@ -1,17 +1,20 @@
-import { HttpErrorResponse } from '@angular/common/http';
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { forkJoin, takeUntil } from 'rxjs';
+import { UnsubscriptionHandler } from 'src/app/models/classes/unsubscription-handler';
 import { Company, Game, Genre, Mode, Platform } from 'src/app/models/interfaces/game.interface';
+import { GamesService } from 'src/app/providers/services/games.service';
 
 @Component({
   selector: 'app-edit-games-dialog',
   templateUrl: './edit-games-dialog.component.html',
   styleUrls: ['./edit-games-dialog.component.scss']
 })
-export class EditGamesDialogComponent implements OnInit {
+export class EditGamesDialogComponent extends UnsubscriptionHandler implements OnInit {
+  public errorMessages: Array<any> = [];
+
   editGameForm: FormGroup;
-  // public errorMessages: Array<any> = [];
 
   genres: Genre[] = [];
   companies: Company[] = [];
@@ -19,9 +22,12 @@ export class EditGamesDialogComponent implements OnInit {
   modes: Mode[] = [];
 
   constructor(
+    private gamesService: GamesService,
     private dialogRef: MatDialogRef<EditGamesDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: Game
   ) {
+    super();
+
     this.editGameForm = new FormGroup({
       id: new FormControl({ value: null, disabled: true }, [Validators.required]),
       logo: new FormControl({ value: null, disabled: true }, [Validators.required]),
@@ -38,14 +44,21 @@ export class EditGamesDialogComponent implements OnInit {
   }
 
   ngOnInit(): void {
-
     this.editGameForm.setValue(this.data);
 
-    // const genres = this.gamesService.getGenres();
-    // const companies = this.gamesService.getCompanies();
-    // const modes = this.gamesService.getModes();
-    // const platforms = this.gamesService.getPlatforms();
+    const genres = this.gamesService.getGenres();
+    const companies = this.gamesService.getCompanies();
+    const modes = this.gamesService.getModes();
+    const platforms = this.gamesService.getPlatforms();
 
+    forkJoin([genres, companies, modes, platforms]).pipe(takeUntil(this.destroy$)).subscribe({
+      next: results => {
+        this.genres = results[0];
+        this.companies = results[1];
+        this.modes = results[2];
+        this.platforms = results[3];
+      }
+    });
   }
 
   onSubmit() {
