e2cf90ae3ace15b8d88a78a8627988d19799c480
Lorenzo Mazza <hello@lorenzomazza.dev>
2022-08-30 12:31:51 +0200

    LOGO
    base64

--- FortGames.Web/src/app/components/toolbar/toolbar.component.ts
+++ FortGames.Web/src/app/components/toolbar/toolbar.component.ts
@@ -1,9 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewChecked, Component, DoCheck, Injectable, Input, OnInit, ViewChild } from '@angular/core';
 import { AuthenticationService } from 'src/app/providers/services/authentication.service';
 import { Router } from '@angular/router';
 import { MatDrawer } from '@angular/material/sidenav';
 import { LogoutComponent } from 'src/app/features/logout/logout.component';
 import { MatDialog } from '@angular/material/dialog';
+import { UsersService } from 'src/app/providers/services/users.service';
+import { User } from 'src/app/models/interfaces/users.interface';
+import { Register } from 'src/app/models/interfaces/register.interface';
+import { AccountComponent } from 'src/app/features/account/account.component';
 
 @Component({
   selector: 'app-toolbar',
@@ -12,15 +16,31 @@ import { MatDialog } from '@angular/material/dialog';
 })
 export class ToolbarComponent implements OnInit {
 
-  isAuthenticated = false;
-  userName!: string | null;
+  @ViewChild(AccountComponent) AccountComponent!: AccountComponent;
   @Input() drawer!: MatDrawer;
 
-  constructor(private authService: AuthenticationService, private router: Router, public dialog: MatDialog) { }
+  isAuthenticated = false;
+  userName!: string | null;
+  currentUser!: Register;
+
+  constructor(
+    private authService: AuthenticationService,
+    public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.authService.isAuthenticated$.subscribe(r => this.isAuthenticated = r);
     this.authService.userName$.subscribe(r => this.userName = r);
+    this.getAccount();
+  }
+
+  getAccount() {
+    this.authService.account().subscribe({
+      next: (r: Register) => {
+        this.currentUser = r;
+        console.log("username: " + this.currentUser.userName);
+        this.authService.userName = r.userName;
+      }
+    });
   }
 
   openDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
